fe5b32c92164b569b0ca5a96054a1ce959b59dd9
Noah Day <sheltielover432@gmail.com>
2019-06-25 12:14:22 -0400

    Update main.dart

--- Resume_Website/lib/main.dart
+++ Resume_Website/lib/main.dart
@@ -4,14 +4,14 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  @override
+//  @overrideweb
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Noah Day Resume'),
     );
   }
 }
@@ -26,6 +26,7 @@ class MyHomePage extends StatelessWidget {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -51,7 +52,7 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Hello, World!',
+              'Hello, My World!',
             ),
           ],
         ),
