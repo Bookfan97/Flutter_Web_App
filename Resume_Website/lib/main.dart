ca26acb98fd798888d21e907672a8a9dca37abea
Noah Day <sheltielover432@gmail.com>
2019-06-26 15:37:49 -0400

    Update
    Listview created

--- Resume_Website/lib/main.dart
+++ Resume_Website/lib/main.dart
@@ -31,32 +31,72 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (choose the "Toggle Debug Paint" action
-          // from the Flutter Inspector in Android Studio, or the "Toggle Debug
-          // Paint" command in Visual Studio Code) to see the wireframe for each
-          // widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello, My World!',
-            ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.map),
+            title: Text('Map'),
+          ),
+          ListTile(
+            leading: Icon(Icons.photo_album),
+            title: Text('Album'),
+          ),
+          ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('Phone'),
+          ),
+              ListTile(
+                title: Text('1625 Main Street',
+                    style: TextStyle(fontWeight: FontWeight.w500)),
+                subtitle: Text('My City, CA 99984'),
+                leading: Icon(
+                  Icons.restaurant_menu,
+                  color: Colors.blue[500],
+                ),
+              ),
+              Divider(),
+              ListTile(
+                title: Text('(408) 555-1212',
+                    style: TextStyle(fontWeight: FontWeight.w500)),
+                leading: Icon(
+                  Icons.contact_phone,
+                  color: Colors.blue[500],
+                ),
+              ),
+              ListTile(
+                title: Text('costa@example.com'),
+                leading: Icon(
+                  Icons.contact_mail,
+                  color: Colors.blue[500],
+                ),
+              ),
+            ],
+          ),
+//      body: Center(
+//        // Center is a layout widget. It takes a single child and positions it
+//        // in the middle of the parent.
+//        child: Column(
+//          // Column is also layout widget. It takes a list of children and
+//          // arranges them vertically. By default, it sizes itself to fit its
+//          // children horizontally, and tries to be as tall as its parent.
+//          //
+//          // Invoke "debug painting" (choose the "Toggle Debug Paint" action
+//          // from the Flutter Inspector in Android Studio, or the "Toggle Debug
+//          // Paint" command in Visual Studio Code) to see the wireframe for each
+//          // widget.
+//          //
+//          // Column has various properties to control how it sizes itself and
+//          // how it positions its children. Here we use mainAxisAlignment to
+//          // center the children vertically; the main axis here is the vertical
+//          // axis because Columns are vertical (the cross axis would be
+//          // horizontal).
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: <Widget>[
+//            Text(
+//              'Hello, My World!',
+//            ),
+//          ],
+//        ),
     );
   }
 }
